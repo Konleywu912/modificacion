9db2b9e8e5e3b39fd51ce5cb89e3f101c49e96cc
Konleywu912 <leymanwu912@gmail.com>
2025-04-03 22:36:05 -0600

    Add files via upload

---
+++ clase12/Clase12.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase12;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leymanwu
+ */
+public class Clase12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Metodo para modificar
+        
+        
+        empleado Empleado1 =new empleado(12345,"Carlos","Ingeniero");
+        
+        Empleado1.modificar();
+                
+                    
+            
+            
+        
+            
+            
+        }
+}
+    
+
